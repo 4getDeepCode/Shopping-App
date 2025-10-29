8aad148836858e20211dc02057189fe737f75ee3
Deep Chand <deepc2060@gmail.com>
2025-10-29 14:35:58 +0530

    add common and UI components with config and unauth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,45 +8,59 @@ import AdminOrders from './pages/admin-view/orders'
 import AdminProducts from './pages/admin-view/products'
 import AdminFeatures from './pages/admin-view/features'
 import AdminDashboard from './pages/admin-view/dashboard'
-
 import PageNotFound from './pages/not-found'
 import ShoppingAccount from './pages/shopping-view/account'
 import ShoppingCheckout from './pages/shopping-view/checkout'
 import ShoppingListing from './pages/shopping-view/listing'
 import ShoppingHome from './pages/shopping-view/home'
 import ShoppingLayout from './components/shopping-view/layout'
+import CheckAuth from './components/common/check-auth'
+import UnAuthPage from './pages/unauth-page'
 
 
 function App() {
 
+  const isAuthenticated = false;
+  const user = null;
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1>header component</h1>
-
       <Routes>
 
-        <Route path='/auth' element={<AuthLayout />}>
-          <Route path='login' element={<AuthLogin/>}/>
-          <Route path='register' element={<AuthRegister/>}/>
+        <Route path='/auth' element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+        }>
+          <Route path='login' element={<AuthLogin />} />
+          <Route path='register' element={<AuthRegister />} />
         </Route>
 
 
-        <Route path='/admin' element={<AdminLayout/>}>
-          <Route path='dashboard' element={<AdminDashboard/>}/>
-          <Route path='features' element={<AdminFeatures/>}/>
-          <Route path='products' element={<AdminProducts/>}/>
-          <Route path='orders' element={<AdminOrders/>}/>
+        <Route path='/admin' element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+        }>
+          <Route path='dashboard' element={<AdminDashboard />} />
+          <Route path='features' element={<AdminFeatures />} />
+          <Route path='products' element={<AdminProducts />} />
+          <Route path='orders' element={<AdminOrders />} />
         </Route>
 
-        <Route path='/shop' element={<ShoppingLayout/>}>
-            <Route path='home' element={<ShoppingHome/>}/>
-            <Route path='listing' element={<ShoppingListing/>}/>
-            <Route path='checkout' element={<ShoppingCheckout/>}/>
-            <Route path='account' element={<ShoppingAccount/>}/>
+        <Route path='/shop' element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShoppingLayout />
+          </CheckAuth>
+        }>
+          <Route path='home' element={<ShoppingHome />} />
+          <Route path='listing' element={<ShoppingListing />} />
+          <Route path='checkout' element={<ShoppingCheckout />} />
+          <Route path='account' element={<ShoppingAccount />} />
         </Route>
 
-        <Route path='*' element={<PageNotFound/>}/>
+        <Route path='*' element={<PageNotFound />} />
+        <Route path='/unauth-page' element={<UnAuthPage />} />
 
       </Routes>
 
