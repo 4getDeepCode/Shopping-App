8d0b153ae512045a7fe9c3553e776bbc8677a6b7
Deep Chand <deepc2060@gmail.com>
2025-11-14 11:10:37 +0530

    update auth controller and server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookeiParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRouter = require('./routes/auth/auth-routes');
 
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin : 'http://localhost:5173/',
+        origin : 'http://localhost:5173',
         method : ['GET', 'PUT', 'POST', 'DELETE'],
         allowedHeaders :[
             "Content-Type",
@@ -33,7 +33,7 @@ app.use(
     })
 );
 
-app.use(cookeiParser());
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 
